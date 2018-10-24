87bc35132e43b515515d62ec2ea157ec2e74668a
Zander Horn <zander.horn@stonethree.com>
2018-10-24 09:40:36 +0200

    Volume estimator added, small other changes
    I haven't tested anything, so, eh

--- tank_model.py
+++ tank_model.py
@@ -1,13 +1,16 @@
 # Team Dapper Strategy tank model for SACAC Hackathon 2018
 # Assuming linear level and valve responses
 
-TANK_VOLUME = 5     # m³
-LEVEL = 0.5         # initial tank level fraction
-LEAK_CONST = 0      # leakrate = leakconst*level
-TOTAL_OVERFLOW = 0  # historic waste total
-TOTAL_CONSUMP = 0   # historical total consumption of sources
-SHORTFALL = False   # did the tank experience a water shortfall in the last step
-OVERFLOW = False    # did the tank overflow in the last step
+import numpy as np
+import scipy.optimize as op
+
+TANK_VOLUME = 5.0       # m³
+LEVEL = 0.5             # initial tank level fraction
+LEAK_CONST = 0.0        # leakrate = leakconst*level
+TOTAL_OVERFLOW = 0.0    # historic waste total
+TOTAL_CONSUMP = 0.0     # historical total consumption of sources
+SHORTFALL = False       # did the tank experience a water shortfall in the last step
+OVERFLOW = False        # did the tank overflow in the last step
 
 def stepTank(MW=0, AMW=1, BW=0, ABW=1, RW=0, HU=0, GU=0, updateLevel=True):
     """Perform one timestep of tank simulation.
@@ -24,8 +27,9 @@ def stepTank(MW=0, AMW=1, BW=0, ABW=1, RW=0, HU=0, GU=0, updateLevel=True):
     HU -- home usage                            (m³/h)
     updateLevel -- update global level var      (bool)
     """
-
-    TOTAL_CONSUMP = HU + GU
+    global LEVEL, TOTAL_CONSUMP, TOTAL_OVERFLOW, SHORTFALL, OVERFLOW
+    
+    TOTAL_CONSUMP += HU + GU
     accumulation = (MW*AMW + BW*ABW + RW) -\
                    (TOTAL_CONSUMP + LEVEL*LEAK_CONST)
     newVolume = TANK_VOLUME*LEVEL + accumulation
@@ -49,8 +53,12 @@ def stepTank(MW=0, AMW=1, BW=0, ABW=1, RW=0, HU=0, GU=0, updateLevel=True):
 
     return newLevel
 
-def tankVolumeEstimator(HMW = [1], HBW=[1], HHU=[1], HGU=[1], HLEVEL=[0.5], updateVolume=True):
+def tankVolumeEstimator(HMW=[1], HBW=[1], HHU=[1], HGU=[1], HLEVEL=[0.5], updateVolume=True):
     """Estimate tank volume from Historical data.
+    Assumes no underflow, overflow or leakage
+
+    Assuming one set of values is from the last hour, i.e. flows for
+    last hour and level at end
 
     Keyword arguments:
     HMW -- historical municipal water usage     (array m³/h)
@@ -61,10 +69,29 @@ def tankVolumeEstimator(HMW = [1], HBW=[1], HHU=[1], HGU=[1], HLEVEL=[0.5], upda
     updateVolume -- upgate global volume var    (bool)
     """
 
-    #TODO
+    global TANK_VOLUME
+    volGuess = 5
 
-    newVolume = 9000
+    # estimate average disturbance for measurement
+    supply = np.array(HMW) + np.array(HBW)
+    consumption = np.array(HHU) + np.array(HGU)
+    disturbance = (sum(supply) - sum(consumption))/len(HMW)
+
+    # get relevant changes
+    tankChange = (supply - consumption) + disturbance
+    tankChange = tankChange[1:]
+    levelChange = HLEVEL[1:] - HLEVEL[:-1]
+
+    def tankFunc(V):
+        output = []
+        for i in len(tankChange):
+            out = V - tankChange[i]/levelChange[i]
+            output.append(out)
+        return output
+
+    newVolume = op.fsolve(tankFunc, volGuess)
+
     if updateVolume:
         TANK_VOLUME = newVolume
-        
+
     return newVolume
