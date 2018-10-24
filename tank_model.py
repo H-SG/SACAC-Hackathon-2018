016e4598415e7fb8d9f3f81e8941f1403d4844fa
Zander Horn <zander.horn@stonethree.com>
2018-10-24 10:52:49 +0200

    Fixes, etc

--- tank_model.py
+++ tank_model.py
@@ -1,48 +1,50 @@
 # Team Dapper Strategy tank model for SACAC Hackathon 2018
 # Assuming linear level and valve responses
 
-import numpy as np
-import scipy.optimize as op
-
 TANK_VOLUME = 5.0       # m³
 LEVEL = 0.5             # initial tank level fraction
+LMIN = 0.05             # minimum level setting
+LMAX = 0.95             # maximum level setting
 LEAK_CONST = 0.0        # leakrate = leakconst*level
-TOTAL_OVERFLOW = 0.0    # historic waste total
+TOTAL_OVERFLOW = 0.0    # historical waste total
+TOTAL_SHORTFAL = 0.0    # historical total water shortfall
 TOTAL_CONSUMP = 0.0     # historical total consumption of sources
 SHORTFALL = False       # did the tank experience a water shortfall in the last step
 OVERFLOW = False        # did the tank overflow in the last step
 
-def stepTank(MW=0, AMW=1, BW=0, ABW=1, RW=0, HU=0, GU=0, updateLevel=True):
-    """Perform one timestep of tank simulation.
+def stepTank(MW=0, MWMax=1, BW=0, BWMax=1, RW=0, HU=0, GU=0, updateLevel=True):
+    """Perform one timestep of tank simulation and return new level.
 
-    This will return a level reading, other parameters
-    will be stored in global module vars.
+    Other parameters will be stored in global module vars.
 
     Keyword arguments:
     MW -- municipal water valve setting         (fraction)
-    AMW -- municipal water current max supply   (m³/h)
+    MWMax -- municipal water max supply         (m³/h)
     BW -- borehole water valve setting          (fraction)
-    ABW -- borehole water current max supply    (m³/h)
+    BWMax -- borehole water current max supply  (m³/h)
     RW -- rainwater flow into tank              (m³/h)
     HU -- home usage                            (m³/h)
     updateLevel -- update global level var      (bool)
     """
-    global LEVEL, TOTAL_CONSUMP, TOTAL_OVERFLOW, SHORTFALL, OVERFLOW
-    
-    TOTAL_CONSUMP += HU + GU
-    accumulation = (MW*AMW + BW*ABW + RW) -\
-                   (TOTAL_CONSUMP + LEVEL*LEAK_CONST)
+    global LEVEL, TOTAL_CONSUMP, TOTAL_OVERFLOW, TOTAL_SHORTFAL,\
+           SHORTFALL, OVERFLOW
+
+    consumption = HU + GU
+    TOTAL_CONSUMP += consumption
+    accumulation = (MW*MWMax + BW*BWMax + RW) -\
+                   (consumption + LEVEL*LEAK_CONST)
     newVolume = TANK_VOLUME*LEVEL + accumulation
 
-    if newVolume > TANK_VOLUME:
-        TOTAL_OVERFLOW += TANK_VOLUME - newVolume
+    if newVolume > TANK_VOLUME*LMAX:
+        TOTAL_OVERFLOW += newVolume - TANK_VOLUME*LMAX
         SHORTFALL = False
         OVERFLOW = True
-        newLevel = 1        
-    elif newVolume < 0:
+        newLevel = LMAX
+    elif newVolume < TANK_VOLUME*LMIN:
+        TOTAL_SHORTFAL += TANK_VOLUME*LMIN - newVolume
         SHORTFALL = True
-        OVERFLOW = True
-        newLevel = 0
+        OVERFLOW = False
+        newLevel = LMIN
     else:
         SHORTFALL = False
         OVERFLOW = False
@@ -53,45 +55,39 @@ def stepTank(MW=0, AMW=1, BW=0, ABW=1, RW=0, HU=0, GU=0, updateLevel=True):
 
     return newLevel
 
-def tankVolumeEstimator(HMW=[1], HBW=[1], HHU=[1], HGU=[1], HLEVEL=[0.5], updateVolume=True):
-    """Estimate tank volume from Historical data.
-    Assumes no underflow, overflow or leakage
-
-    Assuming one set of values is from the last hour, i.e. flows for
-    last hour and level at end
+def simTank(MW=[1], BW=[1], HU=[1], GU=[1], RW=[1], LInit=0.5):
+    """Calculate total waste and underflow volumes for specified data
 
     Keyword arguments:
-    HMW -- historical municipal water usage     (array m³/h)
-    HBW -- historical borehole water usage      (array m³/h)
-    HHU -- historical home water usage          (array m³/h)
-    HGU -- historical garden water usage        (array m³/h)
-    HLEVEL -- historical tank level             (array m³/h)
-    updateVolume -- upgate global volume var    (bool)
+    MW --  municipal water usage     (array m³/h)
+    BW --  borehole water usage      (array m³/h)
+    HU --  home water usage          (array m³/h)
+    GU --  garden water usage        (array m³/h)
+    RW --  rainwater                 (array m³/h)
+    LInit -- initial tank level      (fraction)
     """
 
-    global TANK_VOLUME
-    volGuess = 5
+    totOverflow = 0
+    totUnderflow = 0
+    totConsumption = 0
 
-    # estimate average disturbance for measurement
-    supply = np.array(HMW) + np.array(HBW)
-    consumption = np.array(HHU) + np.array(HGU)
-    disturbance = (sum(supply) - sum(consumption))/len(HMW)
+    for i in range(0, len(MW)):
+        totConsumption += MW[i] + BW[i]
 
-    # get relevant changes
-    tankChange = (supply - consumption) + disturbance
-    tankChange = tankChange[1:]
-    levelChange = HLEVEL[1:] - HLEVEL[:-1]
+        tankBal = (MW[i] + BW[i] + RW[i]) - (HU[i] + GU[i])
 
-    def tankFunc(V):
-        output = []
-        for i in len(tankChange):
-            out = V - tankChange[i]/levelChange[i]
-            output.append(out)
-        return output
+        if not i:
+            currLevel = LInit
+        
+        newVolume = TANK_VOLUME*currLevel + tankBal
 
-    newVolume = op.fsolve(tankFunc, volGuess)
+        if newVolume > TANK_VOLUME*LMAX:
+            currLevel = LMAX
+            totOverflow += newVolume - TANK_VOLUME*LMAX
+        elif newVolume < TANK_VOLUME*LMIN:
+            currLevel = LMIN
+            totUnderflow += TANK_VOLUME*LMIN - newVolume
+        else:
+            currLevel = newVolume/TANK_VOLUME
 
-    if updateVolume:
-        TANK_VOLUME = newVolume
-
-    return newVolume
+    return totOverflow, totUnderflow, totConsumption
