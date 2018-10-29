62403184cc389d773914210b2a0dcfbc87576aa0
Zander Horn <zander.horn@gmail.com>
2018-10-29 14:33:35 +0200

    Good Controller

--- SACAC Code/Controller.py
+++ SACAC Code/Controller.py
@@ -1,28 +1,33 @@
 def Controller(L, AMW, ABW, HU, GU):
-    """AMW, ABW, HU, GU refering to previous time step values"""
-
-    # Set relevant model parameters -------------------------------------------
     MWmax = 0.18
     BWmax = 0.1
-    Lmax = 2.155
-    Lmin = 0
-    ## Set setpoint -----------------------------------------------------------
-    sp = {"Emptier": 0.2, "Half-full": 0.5, "Fuller": 0.8}
-    level = "Emptier"
+    global tStep, bigEnd
+    tStep += 1
 
-    Lsp = sp[level] * Lmax
+    V = (2.155*4.641)
+    L = L/2.155
+    SP = 0.4
+    ER  = SP - L[0]
 
-    ## Very simple controller -------------------------------------------------
-    # If level too low: If AMW==1; MW = MWmax; Else BW = BWmax
-    if L[0] < Lsp:
-        if AMW[0] == 1:
-            MWspec = MWmax
-            BWspec = 0
+    retMW = 0
+    retBW = 0
+
+    if ER > 0.05:
+        if L[0] == 0:
+            retMW = MWmax
+            retBW = BWmax
         else:
-            MWspec = 0
-            BWspec = BWmax
-    else:
-        MWspec = 0
-        BWspec = 0
+            retMW = MWmax
+    
+    if (tStep > 8400):
+        if L[0] > 0.75:
+            bigEnd = True
+        if bigEnd & (L[0] > 0):
+            return 0, 0
+        else:
+            return retMW, retBW
+    
+    return retMW, retBW
 
-    return MWspec, BWspec
+tStep = 0
+bigEnd = False
